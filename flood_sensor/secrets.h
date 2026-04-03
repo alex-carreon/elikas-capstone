1570c4aeaa535b3aca28b5f84afbe885f13b6305
Alex Carreon <maalexandra.carreon@benilde.edu.ph>
2026-04-04 04:33:58 +0800

    move .gitignore

--- flood_sensor/secrets.h
+++
@@ -1,6 +0,0 @@
-#define SECRET_API_KEY    "your_api_key_here"
-#define SECRET_SERVER "https://webhook.site/"
-#define SECRET_SENSOR_ID  "TEST_001"
-
-#define SECRET_SSID "GlobeAtHome_87898_2.4"
-#define SECRET_PASS "your_password"
