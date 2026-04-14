11a2c001b6bb24be248d832d6091d14900bc2a5e
bloobry <hsgamoras@gmail.com>
2026-04-14 13:29:58 +0800

    Components and Pages
    Added more components + tsx pages for registration

---
+++ elikas-frontend/src/Authentication/FormRegistration.tsx
@@ -0,0 +1,94 @@
+import { Link } from "react-router-dom";
+import Logo from "../components/Logo";
+import { ArrowLeftIcon } from "lucide-react";
+import colors from "@/constants/colors";
+import TextField from "@/components/TextField";
+import ButtonComp from "@/components/Button";
+
+function FormRegistration() {
+  return (
+    <div className="min-h-screen flex justify-center p-6">
+      <div className="w-full max-w-sm flex justify-evenly flex-col">
+        <div className="mb-6">
+          <Link to="/Registration/Splash" id="R-BackSplash">
+            <ArrowLeftIcon />
+          </Link>
+        </div>
+        <div className="flex justify-center mb-6">
+          <Logo />
+        </div>
+        <div className="flex justify-center flex-col">
+          <h1
+            className="font-BeVietnamPro text-2xl text-center font-bold"
+            style={{ color: colors.heading }}
+          >
+            Create your Account
+          </h1>
+          <p
+            className="font-BeVietnamPro text-sm text-center p-1"
+            style={{ color: colors.heading }}
+          >
+            Fill all the needed information
+          </p>
+        </div>
+        <div className="w-full max-w-xs flex justify-start flex-col self-center gap-5 mt-10 mb-10">
+          <TextField
+            label="Last Name"
+            placeholder="Enter your last name"
+            inputType="text"
+            id="R-LNfield"
+          />
+          <TextField
+            label="First Name"
+            placeholder="Enter your first name"
+            inputType="text"
+            id="R-FNfield"
+          />
+          <TextField
+            label="Email Address"
+            placeholder="Enter your email address"
+            inputType="text"
+            id="R-EMAILfield"
+          />
+          <TextField
+            label="City"
+            placeholder="Enter your current city of residence"
+            inputType="text"
+            id="R-CITYfield"
+          />
+          <TextField
+            label="Barangay"
+            placeholder="Enter your current barangay of residence"
+            inputType="text"
+            id="R-BRGYfield"
+          />
+          <TextField
+            label="Password"
+            placeholder="Enter your password of choice"
+            inputType="password"
+            isPassword
+            id="R-PWfield"
+          />
+          <TextField
+            label="Confirm Password"
+            placeholder="Re-enter your password"
+            inputType="password"
+            isPassword
+            id="R-PWfield"
+          />
+        </div>
+        <div className="w-full flex justify-center items-center m-0">
+          <Link to="/" className="w-full max-w-xs">
+            <ButtonComp
+              text="Next"
+              variant="primary"
+              id="R-FormSubmit"
+            ></ButtonComp>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FormRegistration;
