bd4ff42e758710af545b42c59383e88fb0303c02
bloobry <hsgamoras@gmail.com>
2026-04-15 17:13:15 +0800

    Simple Validations + Form Submission
    Added UI Validations - to wait for backend to test out validations and submissions

--- elikas-frontend/src/Authentication/ContactNo.tsx
+++ elikas-frontend/src/Authentication/ContactNo.tsx
@@ -3,9 +3,29 @@ import Logo from "@/components/Logo";
 import TextField from "@/components/TextField";
 import colors from "@/constants/colors";
 import { ArrowLeftIcon } from "lucide-react";
+import { useState } from "react";
 import { Link } from "react-router";
 
 function ContactNo() {
+  const [contact, setContact] = useState("");
+  const [errors, setErrors] = useState({ contact: "" });
+
+  const contactValidate = /^639\d{9}$/;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(contact);
+
+    if (!contactValidate.test(contact)) {
+      setErrors({
+        contact: "Invalid Contact Number",
+      });
+    } else
+      setErrors({
+        contact: "",
+      });
+  };
+
   return (
     <div className="min-h-screen flex justify-center p-6">
       <div className="w-full max-w-sm flex flex-col">
@@ -18,48 +38,57 @@ function ContactNo() {
           <Logo />
         </div>
         <div className="h-full flex justify-between flex-col">
-          <div className="h-1/2 flex justify-evenly flex-col">
-            <div>
-              <h1
-                className="BeVietnamPro text-2xl text-center font-bold"
-                style={{ color: colors.heading }}
-              >
-                Receive Updates from your Local Barangays!
-              </h1>
-              <p
-                className="text-sm text-center p-2"
-                style={{ color: colors.heading }}
-              >
-                Enter your Phone Number in order to receive the latest updates
-                from your barangays themselves.{" "}
-                <b>You may enter your contact number later.</b>
-              </p>
+          <form
+            onSubmit={handleSubmit}
+            className="h-full flex justify-between flex-col mt-8"
+          >
+            <div className="h-1/2 flex justify-evenly flex-col">
+              <div>
+                <h1
+                  className="BeVietnamPro text-2xl text-center font-bold"
+                  style={{ color: colors.heading }}
+                >
+                  Receive Updates from your Local Barangays!
+                </h1>
+                <p
+                  className="text-sm text-center p-2"
+                  style={{ color: colors.heading }}
+                >
+                  Enter your Phone Number in order to receive the latest updates
+                  from your barangays themselves.{" "}
+                  <b>You may enter your contact number later.</b>
+                </p>
+              </div>
+              <div className="flex justify-center">
+                <TextField
+                  label="Contact Number"
+                  placeholder="639081057526"
+                  inputType="tel"
+                  id="R-ContactNo"
+                  onSubmit={(e) => setContact(e.target.value)}
+                  isRequired
+                  error={errors.contact}
+                />
+              </div>
             </div>
-            <div className="flex justify-center">
-              <TextField
-                label="Contact Number"
-                placeholder="639081057526"
-                inputType="tel"
-                id="R-ContactNo"
-              />
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center m-0 flex-col gap-2">
-            <Link to="/Registration/Verify" className="w-full max-w-xs">
+            <div className="w-full flex justify-center items-center m-0 flex-col gap-2">
+              {/* <Link to="/Registration/Verify" className="w-full max-w-xs"> */}
               <ButtonComp
                 text="Next"
                 variant="primary"
                 id="R-FormSubmit"
+                type="submit"
               ></ButtonComp>
-            </Link>
-            <Link to="/Registration/Verify" className="w-full max-w-xs">
-              <ButtonComp
-                text="Skip"
-                variant="outline"
-                id="R-FormSubmit"
-              ></ButtonComp>
-            </Link>
-          </div>
+              {/* </Link> */}
+              <Link to="/Registration/Verify" className="w-full max-w-xs">
+                <ButtonComp
+                  text="Skip"
+                  variant="outline"
+                  id="R-FormSubmit"
+                ></ButtonComp>
+              </Link>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- elikas-frontend/src/Authentication/LogIn.tsx
+++ elikas-frontend/src/Authentication/LogIn.tsx
@@ -7,8 +7,24 @@ import ButtonComp from "../components/Button";
 import { Mail } from "lucide-react";
 import { Lock } from "lucide-react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function LogIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({ email: "", password: "" });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, password);
+
+    //Call API here - try !response.ok return error, else redirect, catch server error
+    // setErrors({
+    //   email: "Invalid email or password",
+    //   password: "Invalid email or password",
+    // });
+  };
+
   return (
     <>
       <div className="min-h-screen flex justify-center p-6">
@@ -32,45 +48,57 @@ function LogIn() {
               A Community-Centered Disaster Information System
             </p>
           </div>
-          <div className="w-full max-w-xs flex justify-start flex-col content-center mx-auto">
-            <div className="flex justify-start flex-col content-center gap-5">
-              <TextField
-                label="Email"
-                placeholder="Enter your email"
-                icon={Mail}
-                inputType="email"
-                id="L-EmailField"
-              ></TextField>
-              <TextField
-                label="Password"
-                placeholder="********"
-                icon={Lock}
-                inputType="password"
-                isPassword
-                id="L-PasswordField"
-              ></TextField>
+          <form
+            onSubmit={handleSubmit}
+            className="h-1/3 flex justify-between flex-col"
+          >
+            <div className="w-full max-w-xs flex justify-start flex-col content-center mx-auto">
+              <div className="flex justify-start flex-col content-center gap-5">
+                <TextField
+                  label="Email"
+                  placeholder="Enter your email"
+                  icon={Mail}
+                  inputType="email"
+                  id="L-EmailField"
+                  isRequired
+                  onSubmit={(e) => setEmail(e.target.value)}
+                  error={errors.email}
+                ></TextField>
+                <TextField
+                  label="Password"
+                  placeholder="********"
+                  icon={Lock}
+                  inputType="password"
+                  isPassword
+                  id="L-PasswordField"
+                  isRequired
+                  onSubmit={(e) => setPassword(e.target.value)}
+                  error={errors.password}
+                ></TextField>
+              </div>
+              <div className="flex mt-2 flex-row justify-between">
+                <CheckBox text="Remember for 30 days" id="L-Remember" />
+                <Link
+                  to="/"
+                  className={"text-xs underline"}
+                  style={{ color: colors.label }}
+                  id="L-ForgotPassword"
+                >
+                  Forgot Password
+                </Link>
+              </div>
             </div>
-            <div className="flex mt-2 flex-row justify-between">
-              <CheckBox text="Remember for 30 days" id="L-Remember" />
-              <Link
-                to="/"
-                className={"text-xs underline"}
-                style={{ color: colors.label }}
-                id="L-ForgotPassword"
-              >
-                Forgot Password
-              </Link>
-            </div>
-          </div>
-          <div className="w-full flex justify-center items-center m-0">
-            <Link to="/" className="w-full max-w-xs">
+            <div className="w-full flex justify-center items-center m-0">
+              {/* <Link to="/" className="w-full max-w-xs"> */}
               <ButtonComp
                 text="Log In"
                 variant="primary"
+                type="submit"
                 id="L-Submit"
               ></ButtonComp>
-            </Link>
-          </div>
+              {/* </Link> */}
+            </div>
+          </form>
           <div className="flex justify-start flex-col content-center mx-auto">
             <p className={"text-sm flex justify-self-center"}>
               Don't have an account yet? &nbsp;
