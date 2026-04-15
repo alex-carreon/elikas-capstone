5cdc46d24c2aff498831b4032886286cb6622346
bloobry <hsgamoras@gmail.com>
2026-04-15 17:13:15 +0800

    Simple Validations + Form Submission
    Added UI Validations - to wait for backend to test out validations and submissions

--- elikas-frontend/src/components/Button.tsx
+++ elikas-frontend/src/components/Button.tsx
@@ -6,15 +6,17 @@ interface ButtonProps {
   variant: "primary" | "outline";
   onClick?: () => void;
   id: string;
+  type?: "button" | "submit" | "reset";
 }
 
-function ButtonComp({ text, variant, onClick, id }: ButtonProps) {
+function ButtonComp({ text, variant, onClick, id, type }: ButtonProps) {
   if (variant === "primary") {
     return (
       <Button
         onClick={onClick}
         className="size-lg w-full max-w-xs h-10 bg-gradient-to-r from-[#FFA011] to-[#F3C962]"
         id={id}
+        type={type}
       >
         {text}
       </Button>
