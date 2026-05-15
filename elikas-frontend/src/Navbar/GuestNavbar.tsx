55b994d0b6d319cd4eb6a164a00e7d8feee27976
bloobry <hsgamoras@gmail.com>
2026-05-15 13:51:25 +0800

    logout alert
    Logout Alert - whenever user is in guest page it shows that they r logged out

--- elikas-frontend/src/Navbar/GuestNavbar.tsx
+++ elikas-frontend/src/Navbar/GuestNavbar.tsx
@@ -3,11 +3,16 @@ import LogoComp from "../components/Logo";
 import ButtonComp from "@/components/Button";
 import { Phone } from "lucide-react";
 import colors from "@/constants/colors";
+import { Button } from "@/components/ui/button";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { useState } from "react";
 
-function Navbar() {
+function GuestNavbar() {
+  const [closeAlert, setCloseAlert] = useState(false);
+
   return (
     <>
-      <div className="fixed top-0 left-0 z-50 w-full h-content flex justify-center">
+      <div className="fixed top-0 left-0 z-50 w-full h-content flex flex-col justify-center items-center gap-3">
         <div className="w-full max-w-md flex flex-row justify-between items-center p-3 shadow-lg bg-white">
           <Link to="/Login">
             <div>
@@ -29,10 +34,25 @@ function Navbar() {
             </div>
           </Link>
         </div>
+        {closeAlert ? null : (
+          <Alert className="w-full max-w-sm py-4 shadow-lg bg-[#FFF1DD] text-center flex flex-col items-center gap-3">
+            {/* <CheckCircle2Icon /> */}
+            <AlertTitle className="font-bold" style={{ color: colors.heading }}>
+              You are logged out!
+            </AlertTitle>
+            <AlertDescription style={{ color: colors.heading }}>
+              You are now in guest mode. You can still explore the map, but
+              you’ll need an account to join the conversation.
+            </AlertDescription>
+            <Button className="w-2/3" onClick={() => setCloseAlert(true)}>
+              Got it!
+            </Button>
+          </Alert>
+        )}
       </div>
       <Outlet />
     </>
   );
 }
 
-export default Navbar;
+export default GuestNavbar;
