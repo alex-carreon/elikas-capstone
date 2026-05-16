e6e7d92da40cfd4ca9657af3620d9d2fe6140994
bloobry <hsgamoras@gmail.com>
2026-05-16 16:43:34 +0800

    Fixed button comp and form ui
    - Added height and width as properties for button
    - Multi select widt

--- elikas-frontend/src/components/Button.tsx
+++ elikas-frontend/src/components/Button.tsx
@@ -8,7 +8,8 @@ interface ButtonProps {
   id: string;
   type?: "button" | "submit" | "reset";
   isDisabled?: boolean;
-  size?: "small" | "medium" | "large";
+  widthSize?: string;
+  heightSize?: string;
 }
 
 function ButtonComp({
@@ -18,13 +19,14 @@ function ButtonComp({
   id,
   type,
   isDisabled,
-  size,
+  widthSize,
+  heightSize,
 }: ButtonProps) {
   if (variant === "primary") {
     return (
       <Button
         onClick={onClick}
-        className={`w-full max-w-xs h-10 bg-gradient-to-r from-[#FFA011] to-[#F3C962]`}
+        className={`w-${widthSize} max-w-xs h-${heightSize} grow bg-gradient-to-r from-[#FFA011] to-[#F3C962]`}
         id={id}
         type={type}
         disabled={isDisabled}
@@ -48,7 +50,7 @@ function ButtonComp({
     return (
       <Button
         onClick={onClick}
-        className="size-lg w-2xs max-w-xs h-10 bg-white p-6"
+        className={`size-lg w-${widthSize} max-w-xs h-${heightSize} bg-white grow`}
         style={{ background: colors.heading, color: "white" }}
         id={id}
         disabled={isDisabled}
