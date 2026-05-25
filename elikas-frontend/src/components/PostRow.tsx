0647774a41fe0e2db7cee5730f0d8f1c9617b63a
bloobry <hsgamoras@gmail.com>
2026-05-25 18:37:55 +0800

    Added avatar icon
    Added avatar icon thats it what do u weant from me

--- elikas-frontend/src/components/PostRow.tsx
+++ elikas-frontend/src/components/PostRow.tsx
@@ -6,6 +6,9 @@ import {
 } from "@/components/ui/collapsible";
 import colors from "@/constants/colors";
 import { useState } from "react";
+import { bigSmile } from "@dicebear/collection";
+import { createAvatar } from "@dicebear/core";
+import { data } from "react-router";
 
 interface PostRowProps {
   username: string;
@@ -32,6 +35,19 @@ function PostRow({
 }: PostRowProps) {
   const [report, setReport] = useState(false);
   const [vote, setVote] = useState<"up" | "down" | null>(null);
+  const [seed, setSeed] = useState("Felix");
+
+  const avatar = createAvatar(bigSmile, {
+    seed: seed,
+    backgroundColor: ["b6e3f4", "c0aede", "d1d4f9"],
+    radius: 50,
+    scale: 90,
+    accessoriesProbability: 50,
+    eyes: ["cheery", "normal", "starstruck", "winking"],
+    mouth: ["braces", "gapSmile", "kawaii", "openedSmile", "teethSmile"],
+  });
+
+  const dataUri = avatar.toDataUri();
 
   return (
     <div className="mt-1 flex gap-1 flex-col">
@@ -51,16 +67,21 @@ function PostRow({
         )}
         <p>Report</p>
       </div>
-      <div className="bg-[#B6D6FF] p-4 rounded-lg flex gap-0.5 flex-col">
-        <div className="flex flex-row justify-between">
-          <p>
-            <b>{username}</b>
-          </p>
-          <p className="text-xs" style={{ color: colors.label }}>
-            {timePosted}
-          </p>
+      <div className="bg-[#B6D6FF] p-3 rounded-lg flex flex-row">
+        <div className="w-full flex flex-row gap-2">
+          <img src={dataUri} className="w-10" />
+          <div className="w-full flex flex-col">
+            <div className="flex flex-row justify-between">
+              <p>
+                <b>{username}</b>
+              </p>
+              <p className="text-xs" style={{ color: colors.label }}>
+                {timePosted}
+              </p>
+            </div>
+            <p className="text-xs">{description}</p>
+          </div>
         </div>
-        <p className="text-xs">{description}</p>
       </div>
       <Collapsible className="">
         <div className="px-2 flex flex-row gap-4">
