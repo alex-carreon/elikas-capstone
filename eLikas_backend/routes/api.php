c8833b98d8ba4ffff6e7deb521a255ed7c225dda
kurtythinks <kurthacinas@gmail.com>
2026-05-28 04:01:30 +0800

    added update user in dashboard

--- eLikas_backend/routes/api.php
+++ eLikas_backend/routes/api.php
@@ -87,8 +87,11 @@ Route::middleware(['firebase.auth', 'role:2'])->group(function () {
 
 // BARANGAY OR ADMIN ROUTES
 Route::middleware(['firebase.auth', 'role:1,2'])->group(function () {
+    //USER DASHBOARD
     Route::get('/admin/users', [UserController::class, 'allUsers']);
-    Route::get('admin/users/{id}', [UserController::class, 'getUser']);
+    Route::get('/admin/users/{id}', [UserController::class, 'getUser']);
+    Route::patch('/admin/users/{id}', [UserController::class, 'updateUser']); //can update any user
+
     Route::get('/sensors', [SensorController::class, 'index']);
     Route::get('/sensors/{sensor}', [SensorController::class, 'show']);
 
