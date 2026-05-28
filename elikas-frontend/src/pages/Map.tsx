ddfbad238fcba3c40a079e45cfd39a3f011867d0
bloobry <hsgamoras@gmail.com>
2026-05-28 20:51:35 +0800

    Map Filter - Hazards
    Map Filter Hazards works yey!

--- elikas-frontend/src/pages/Map.tsx
+++ elikas-frontend/src/pages/Map.tsx
@@ -1,15 +1,19 @@
 import MapComp from "@/components/Map";
 import "leaflet/dist/leaflet.css";
 import Filter from "@/components/Filter";
+import { useState } from "react";
+import { MapFilterProvider } from "@/context/MapFilterContext";
 
 function Map() {
   return (
     <div className="flex justify-center pt-13 w-full">
       <div className="max-w-md w-full">
-        {/* <div className="fixed w-full max-w-md"> */}
-        <Filter />
-        {/* </div> */}
-        <MapComp />
+        <MapFilterProvider>
+          {/* <div className="fixed w-full max-w-md"> */}
+          <Filter />
+          {/* </div> */}
+          <MapComp />
+        </MapFilterProvider>
       </div>
     </div>
   );
