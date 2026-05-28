20659453c8fbb69a0b59603f7b1da537ef879150
Alex Carreon <maalexandra.carreon@benilde.edu.ph>
2026-05-29 02:47:22 +0800

    Move sensor-related controllers to SensorControllers folder for file management

--- eLikas_backend/app/Http/Controllers/SensorControllers/SensorController.php
+++ eLikas_backend/app/Http/Controllers/SensorControllers/SensorController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\SensorControllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Sensor;
 use App\Http\Resources\SensorResource;
