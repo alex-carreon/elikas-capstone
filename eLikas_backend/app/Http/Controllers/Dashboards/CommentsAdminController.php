635ace8a784bec168ed4c364623bb66d200446e0
kurtythinks <kurthacinas@gmail.com>
2026-06-03 17:35:22 +0800

    comments dashboard

--- eLikas_backend/app/Http/Controllers/Dashboards/CommentsAdminController.php
+++ eLikas_backend/app/Http/Controllers/Dashboards/CommentsAdminController.php
@@ -4,10 +4,135 @@ namespace App\Http\Controllers\Dashboards;
 
 use App\Http\Controllers\Controller;
 use App\Models\Comment;
+use App\Models\EvacArea;
 use Illuminate\Http\Request;
 
 class CommentsAdminController extends Controller
 {
+
+    /**
+     * GET /admin/evac-areas/{evacAreaId}/comments
+     *
+     * Returns all comments for an evacuation area,
+     * including deactivated comments.
+     */
+    public function index(int $evacAreaId)
+    {
+        $evacArea = EvacArea::with('social_element')
+            ->find($evacAreaId);
+
+        if (!$evacArea) {
+            return response()->json([
+                'message' => 'Evacuation area not found.',
+            ], 404);
+        }
+
+        $comments = Comment::with([
+            'element.user:id,username',
+            'element.media',
+        ])
+        ->where('parent_id', $evacArea->element_id)
+        ->orderByDesc('id')
+        ->get();
+
+        return response()->json([
+            'count' => $comments->count(),
+
+            'comments' => $comments->map(function ($comment) {
+
+                return [
+                    'id' => $comment->id,
+
+                    'content' => $comment->content,
+
+                    'commented_by' => [
+                        'id' => $comment->element?->user?->id,
+                        'username' => $comment->element?->user?->username,
+                    ],
+
+                    'posted_at' => $comment->element?->posted_at
+                        ? $comment->element->posted_at
+                            ->timezone('Asia/Manila')
+                            ->toDateTimeString()
+                        : null,
+
+                    'is_deactivated' =>
+                        !is_null($comment->element?->deactivated_at),
+
+                    'media' => $comment->element?->media
+                        ->map(fn ($m) =>
+                            config('app.media_base_url') . '/' . $m->file_path
+                        )
+                        ->values()
+                        ->toArray() ?? [],
+                ];
+            }),
+        ]);
+    }
+
+    /**
+     * GET /admin/comments/{id}
+     *
+     * Returns full comment details.
+     */
+    public function show(int $id)
+    {
+        $comment = Comment::with([
+            'element.user:id,username',
+            'element.media',
+            'socialElement.evacArea',
+        ])->find($id);
+
+        if (!$comment) {
+            return response()->json([
+                'message' => 'Comment not found.',
+            ], 404);
+        }
+
+        $evacArea = EvacArea::where(
+            'element_id',
+            $comment->parent_id
+        )->first();
+
+        return response()->json([
+            'comment' => [
+
+                'id' => $comment->id,
+
+                'evac_area' => [
+                    'id' => $evacArea?->id,
+                    'name' => $evacArea?->name,
+                ],
+
+                'posted_by' => [
+                    'id' => $comment->element?->user?->id,
+                    'username' => $comment->element?->user?->username,
+                ],
+
+                'content' => $comment->content,
+
+                'upvotes' => $comment->upvotes,
+
+                'downvotes' => $comment->downvotes,
+
+                'posted_at' => $comment->element?->posted_at
+                    ? $comment->element->posted_at
+                        ->timezone('Asia/Manila')
+                        ->toDateTimeString()
+                    : null,
+
+                'is_deactivated' =>
+                    !is_null($comment->element?->deactivated_at),
+
+                'media' => $comment->element?->media
+                    ->map(fn ($m) =>
+                        config('app.media_base_url') . '/' . $m->file_path
+                    )
+                    ->values()
+                    ->toArray() ?? [],
+            ]
+        ]);
+    }
     /**
      * PATCH /admin/comments/{id}
      */
