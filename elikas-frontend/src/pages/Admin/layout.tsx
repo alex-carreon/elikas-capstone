7c920e1fcd51937469ae16d74a7b676c74f27537
bloobry <hsgamoras@gmail.com>
2026-06-06 04:05:58 +0800

    MANY FEATURES combined
    1. Fixed folders
    2. Update Hotlines started - to ask zach
    3. Fixed routing with filters

--- elikas-frontend/src/pages/Admin/layout.tsx
+++ elikas-frontend/src/pages/Admin/layout.tsx
@@ -1,6 +1,6 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import AdminSidebar from "@/components/Navbar/AdminSidebar";
-import AdminNavbar from "./AdminNavbar";
+import AdminNavbar from "@/components/Navbar/AdminNavbar";
 import { Outlet } from "react-router";
 
 function Layout() {
