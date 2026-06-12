77f7d5f6f23c372ffc18723870ad77e4fe66aa0a
kurtythinks <kurthacinas@gmail.com>
2026-06-12 16:43:41 +0800

    lesser restriction in spacing of floodpaths

--- eLikas_backend/app/Services/FloodPathIntersectionService.php
+++ eLikas_backend/app/Services/FloodPathIntersectionService.php
@@ -14,10 +14,7 @@ class FloodPathIntersectionService
         return FloodPath::notExpired()
             ->notDeactivated()
             ->when($excludeId, fn($q) => $q->where('id', '!=', $excludeId))
-            ->whereRaw("ST_Intersects(
-                ST_Buffer(path, 0.0001),
-                ST_Buffer(ST_GeomFromText(?), 0.0001)
-            )", [$candidate->toWkt()])
+            ->whereRaw("ST_Distance(path, ST_GeomFromText(?)) < 0.00005", [$candidate->toWkt()])
             ->exists();
     }
 }
