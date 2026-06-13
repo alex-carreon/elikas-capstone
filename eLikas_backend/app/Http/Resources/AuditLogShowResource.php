ae1d9ed90a14f09a5f2a99ab3007e1fe17527416
Alex Carreon <maalexandra.carreon@benilde.edu.ph>
2026-06-13 18:00:08 +0800

    fix: userName retrieval for show method

--- eLikas_backend/app/Http/Resources/AuditLogShowResource.php
+++ eLikas_backend/app/Http/Resources/AuditLogShowResource.php
@@ -18,7 +18,9 @@ class AuditLogShowResource extends JsonResource
             'id' => $this->id,
             'logId' => $this->log_id,
             'userType' => $this->user_type,
-            'userName' => $this->user ? $this->user->name->first_name . ' ' . $this->user->name->last_name : 'User ID ' . $this->user_id,
+            'userName' => $this->user?->name
+                ? $this->user->name->first_name . ' ' . $this->user->name->last_name
+                : 'User ID ' . $this->user_id,
             'activity' => $this->event,
             'table' => $this->target_table?->table_name,
             'targetId' => $this->target_id,
