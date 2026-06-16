e017a3eeee1c223044215df0a714c492f7345a14
bloobry <195307488+bloobry@users.noreply.github.com>
2026-06-16 17:00:53 +0800

    EXPIRED REMINDERS Modal
    Will change depending on Kurt's end

--- elikas-frontend/src/components/AlertDialogue.tsx
+++ elikas-frontend/src/components/AlertDialogue.tsx
@@ -14,27 +14,33 @@ interface AlertDialogueProps {
   title: string;
   description: string;
   onClick?: (e?: any) => void;
+  onClick2?: (e?: any) => void;
   onClose?: () => void;
   children?: React.ReactNode;
   buttonText: string;
+  buttonText2?: string;
   open: boolean;
   contentId: string;
-  closeId: string;
-  actionId: string;
+  closeId?: string;
+  actionId?: string;
+  actionId2?: string;
   btnType?: "submit" | "button" | "reset" | undefined;
 }
 
 function AlertDialogue({
   onClick,
+  onClick2,
   onClose,
   title,
   description,
   children,
   buttonText,
+  buttonText2,
   open,
   contentId,
   closeId,
   actionId,
+  actionId2,
   btnType,
 }: AlertDialogueProps) {
   return (
@@ -52,27 +58,43 @@ function AlertDialogue({
         >
           <AlertDialogTitle />
           <AlertDialogHeader>
-            <CircleX
-              size={28}
-              fill="#CECECE"
-              strokeWidth={1}
-              className="justify-self-end"
-              onClick={onClose}
-              id={closeId}
-            />
+            {closeId && (
+              <CircleX
+                size={28}
+                fill="#CECECE"
+                strokeWidth={1}
+                className="justify-self-end"
+                onClick={onClose}
+                id={closeId}
+              />
+            )}
             <AlertDialogTitle>{title}</AlertDialogTitle>
             <AlertDialogDescription>{description}</AlertDialogDescription>
           </AlertDialogHeader>
           {children}
           <AlertDialogFooter>
-            <AlertDialogAction
-              className="h-10 w-full"
-              onClick={onClick}
-              id={actionId}
-              type={btnType}
-            >
-              {buttonText}
-            </AlertDialogAction>
+            <div className="w-full flex flex-col items-center gap-2">
+              {actionId && (
+                <AlertDialogAction
+                  className="h-10 w-full"
+                  onClick={onClick}
+                  id={actionId}
+                  type={btnType}
+                >
+                  {buttonText}
+                </AlertDialogAction>
+              )}
+              {actionId2 && (
+                <AlertDialogAction
+                  className="h-10 w-full"
+                  onClick={onClick2}
+                  id={actionId2}
+                  type={btnType}
+                >
+                  {buttonText2}
+                </AlertDialogAction>
+              )}
+            </div>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
