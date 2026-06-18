c1f2f2fd0c56fc161ac9790e1fb6be87dd3aeca8
Alex Carreon <maalexandra.carreon@benilde.edu.ph>
2026-06-19 02:40:11 +0800

    fix: simplify deployment script by removing absolute paths for commands

--- elikas-frontend/.scripts/deploy.sh
+++ elikas-frontend/.scripts/deploy.sh
@@ -4,19 +4,19 @@ set -e
 echo "🚀 Deployment started ..."
 
 # Stop Tailscale Funnel
-sudo /usr/bin/tailscale funnel reset
+tailscale funnel reset
 
 # Navigate to frontend directory
 cd /var/www/elikas-frontend
 
 # Install production dependencies
-/root/.nvm/versions/node/v24.17.0/bin/npm ci 
+npm ci 
 
 # Build app
-/root/.nvm/versions/node/v24.17.0/bin/npm run build
+npm run build
 
 # Reload 
 echo "🔄 Getting eLikas PWA online ..."
-sudo /usr/bin/tailscale funnel --bg 80
+tailscale funnel --bg 80
 
 echo "✅ Deployment finished successfully!"
