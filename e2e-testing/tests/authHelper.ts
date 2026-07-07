853c3a95730e76694b386e802e4f5844d3198daa
Alex Carreon <maalexandra.carreon@benilde.edu.ph>
2026-07-07 15:59:21 +0800

    feat: add loginAsAdmin function for admin user authentication

--- e2e-testing/tests/authHelper.ts
+++ e2e-testing/tests/authHelper.ts
@@ -39,3 +39,23 @@ export async function loginAsIndiv(page: Page): Promise<void> {
   await expect(page).toHaveURL(/\/map$/);
   await expect(page.getByText('Pins Generated!')).toBeVisible({timeout: 60_000});
 }
+
+export async function loginAsAdmin(page: Page): Promise<void> {
+  const testEmail = process.env.ADMIN_EMAIL;  
+  const password = process.env.ADMIN_PASSWORD;  
+
+  await page.goto('/');
+  await expect(page.getByRole('button', { name: 'Sign-in' })).toBeVisible();
+  await expect(page.getByText('Pins Generated!')).toBeVisible({timeout: 60_000});
+
+  await page.getByRole('button', { name: 'Sign-in' }).click();
+  await expect(page.locator('#LogIn_PasswordField')).toBeVisible();
+  await page.locator('#LogIn_EmailField').fill(testEmail as string);
+  await page.locator('#LogIn_PasswordField').fill(password as string);
+  await page.getByRole('button', { name: 'Log In' }).click();
+  
+  // Ensure app loads when logged in
+  await expect(page.getByText('You\'re Logged In!')).toBeVisible({timeout: 60_000});
+  await expect(page).toHaveURL(/\/map$/);
+  await expect(page.getByText('Pins Generated!')).toBeVisible({timeout: 60_000});
+}
